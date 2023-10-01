d376dc6979b069f23344f31aa877aa998ff377bf
xMatysz <matizek56@gmail.com>
2023-10-01 19:12:05 +0200

    feat(Domain): create models

---
+++ src/CollectPay/CollectPay.Domain/Common/Models/AggregateRoot.cs
@@ -0,0 +1,20 @@
+﻿namespace CollectPay.Domain.Common.Models;
+
+public class AggregateRoot : Entity
+{
+    public AggregateRoot(Guid id) : base(id)
+    {
+    }
+
+    protected readonly List<IDomainEvent> _domainEvents = new();
+
+    public List<IDomainEvent> PopDomainEvents()
+    {
+        var copy = _domainEvents.ToList();
+        _domainEvents.Clear();
+
+        return copy;
+    }
+
+    protected AggregateRoot() { }
+}
---
+++ src/CollectPay/CollectPay.Domain/Common/Models/Entity.cs
@@ -0,0 +1,28 @@
+﻿namespace CollectPay.Domain.Common.Models;
+
+public class Entity
+{
+    public Guid Id { get; }
+
+    protected Entity(Guid id)
+    {
+        Id = id;
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (obj is null || obj.GetType() != GetType())
+        {
+            return false;
+        }
+
+        return ((Entity)obj).Id == Id;
+    }
+
+    public override int GetHashCode()
+    {
+        return Id.GetHashCode();
+    }
+
+    protected Entity(){}
+}
