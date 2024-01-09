79af28db5af664ae266da963344fd19cfb220d69
xMatysz <matizek56@gmail.com>
2024-01-09 21:13:31 +0100

    feature: add fontello icon

--- src/CollectPay/CollectionPay.Maui/MauiProgram.cs
+++ src/CollectPay/CollectionPay.Maui/MauiProgram.cs
@@ -19,6 +19,7 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+				fonts.AddFont("fontello.ttf", "Icons");
 			});
 
 		builder.Services.AddSingleton<IShellService, ShellService>();
