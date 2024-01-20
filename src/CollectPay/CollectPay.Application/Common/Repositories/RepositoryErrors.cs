1d97c1dc879544e1db3ce72b3028352143202a75
xMatysz <matizek56@gmail.com>
2024-01-20 19:11:54 +0100

    feat: create RepositoryBase

---
+++ src/CollectPay/CollectPay.Application/Common/Repositories/RepositoryErrors.cs
@@ -0,0 +1,9 @@
+﻿namespace CollectPay.Application.Common.Repositories;
+
+public static class RepositoryErrors
+{
+	public static Error EntityWithIdNotFound(string entityType, string id) => Error.NotFound(
+		code: "EntityWithSpecifiedIdIsNotFound",
+		description: $"{entityType} with {id} is not found");
+
+}
---
+++ src/CollectPay/CollectPay.Infrastructure/Repositories/RepositoryBase.cs
@@ -0,0 +1,13 @@
+﻿using CollectPay.Persistence;
+
+namespace CollectPay.Infrastructure.Repositories;
+
+public abstract class RepositoryBase
+{
+	protected readonly CollectPayDbContext DbContext;
+
+	protected RepositoryBase(CollectPayDbContext dbContext)
+	{
+		DbContext = dbContext;
+	}
+}
