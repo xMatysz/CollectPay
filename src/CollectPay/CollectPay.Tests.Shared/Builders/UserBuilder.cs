f9ea81b2066b1ed6827a8dc1cf354d3bbe735a10
xMatysz <matizek56@gmail.com>
2024-01-23 20:05:52 +0100

    feat: extend UserBuilder

--- src/CollectPay/CollectPay.Tests.Shared/Builders/UserBuilder.cs
+++ src/CollectPay/CollectPay.Tests.Shared/Builders/UserBuilder.cs
@@ -4,8 +4,34 @@ namespace CollectPay.Tests.Shared.Builders;
 
 public class UserBuilder
 {
+	private string _email = TestEmail;
+	private string _password = TestPassword;
+	private string _nickName = TestNick;
+
+	private static string TestEmail => "testEmail@domain.com";
+	private static string TestPassword => "password123";
+	private static string TestNick => "Tester";
+
+	public UserBuilder WithEmail(string email)
+	{
+		_email = email;
+		return this;
+	}
+
+	public UserBuilder WithPassword(string password)
+	{
+		_password = password;
+		return this;
+	}
+
+	public UserBuilder WithNick(string nick)
+	{
+		_nickName = nick;
+		return this;
+	}
+
 	public User Build()
 	{
-		return User.Create("test@domain.com", "pass123", "Tester").Value;
+		return User.Create(_email, _password, _nickName).Value;
 	}
 }
