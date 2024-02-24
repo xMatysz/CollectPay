7dfa31ce5a69156c660a387a142e596b40e85f6b
xMatysz <matizek56@gmail.com>
2024-02-24 16:13:32 +0100

    [WIP]

--- src/CollectPay/CollectPay.Api.ApiTests/Controllers/PaymentControllerTests/WhenSendingRequestToPaymentController.cs
+++ src/CollectPay/CollectPay.Api.ApiTests/Controllers/PaymentControllerTests/WhenSendingRequestToPaymentController.cs
@@ -2,6 +2,7 @@
 using CollectionPay.Contracts.Routes;
 using CollectPay.Api.ApiTests.Common.Doubles;
 using CollectPay.Application.BillAggregate.Queries.GetPayments;
+using CollectPay.Application.Common.Abstraction;
 using CollectPay.Domain.BillAggregate.Entities;
 using ErrorOr;
 
@@ -18,4 +19,38 @@ public class WhenSendingRequestToPaymentController : ControllerTestBase
 		var response = await Client.GetAsync(url);
 		response.StatusCode.Should().Be(HttpStatusCode.OK);
 	}
+
+	[Fact]
+	public async Task ShouldCreatePayment()
+	{
+		const string url = PaymentRoutes.Create;
+		ConfigureHandler<CreatePaymentCommand, ErrorOr<Created>, SuccessFullHandler<CreatePaymentCommand, ErrorOr<Created>>>();
+
+		var response = await Client.GetAsync(url);
+		response.StatusCode.Should().Be(HttpStatusCode.OK);
+	}
+
+	[Fact]
+	public async Task ShouldUpdatePayment()
+	{
+		const string url = PaymentRoutes.Update;
+		ConfigureHandler<UpdatePaymentCommand, ErrorOr<Updated>, SuccessFullHandler<UpdatePaymentCommand, ErrorOr<Updated>>>();
+
+		var response = await Client.GetAsync(url);
+		response.StatusCode.Should().Be(HttpStatusCode.OK);
+	}
+
+	[Fact]
+	public async Task ShouldUpdateRemove()
+	{
+		const string url = PaymentRoutes.Remove;
+		ConfigureHandler<RemovePaymentCommand, ErrorOr<Deleted>, SuccessFullHandler<RemovePaymentCommand, ErrorOr<Deleted>>>();
+
+		var response = await Client.GetAsync(url);
+		response.StatusCode.Should().Be(HttpStatusCode.OK);
+	}
 }
+
+public record CreatePaymentCommand(Guid BillId) : ICommand<Created>;
+public record UpdatePaymentCommand(Guid BillId, Guid PaymentId) : ICommand<Updated>;
+public record RemovePaymentCommand(Guid BillId, Guid PaymentId) : ICommand<Deleted>;
--- src/CollectPay/CollectionPay.Contracts/Routes/PaymentRoutes.cs
+++ src/CollectPay/CollectionPay.Contracts/Routes/PaymentRoutes.cs
@@ -3,4 +3,7 @@
 public static class PaymentRoutes
 {
 	public const string List = "payments";
+	public const string Create = "payment/create";
+	public const string Update = "payment/update";
+	public const string Remove = "payment/remove";
 }
