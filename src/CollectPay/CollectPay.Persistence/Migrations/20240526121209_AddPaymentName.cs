bccfd4ba525e514dfa84989832127a568893eab3
xMatysz <matizek56@gmail.com>
2024-05-26 14:22:53 +0200

    fix: add missing migration

---
+++ src/CollectPay/CollectPay.Persistence/Migrations/20240526121209_AddPaymentName.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CollectPay.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPaymentName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Payments",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Payments");
+        }
+    }
+}
