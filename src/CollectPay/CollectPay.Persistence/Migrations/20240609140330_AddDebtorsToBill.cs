030c536f14f11ffa357da314a6c025b884895539
xMatysz <matizek56@gmail.com>
2024-06-09 16:06:07 +0200

    fix: add migration for adding debtors to bill

---
+++ src/CollectPay/CollectPay.Persistence/Migrations/20240609140330_AddDebtorsToBill.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CollectPay.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDebtorsToBill : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid[]>(
+                name: "Debtors",
+                table: "Bills",
+                type: "uuid[]",
+                nullable: false,
+                defaultValue: new Guid[0]);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Debtors",
+                table: "Bills");
+        }
+    }
+}
